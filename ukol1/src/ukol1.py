5c34c693c0106ba202912b0447d437be318efef5
Jan Franek <janfranek3@gmail.com>
2024-09-12 10:48:57 +0200

    ukol 1

--- ukol1/src/ukol1.py
+++ ukol1/src/ukol1.py
@@ -1,9 +1,41 @@
+import os
 import csv
 
-def  
+# funkce - existuje zaznam s neprazdnym jmenem pro dany objektID?
+def rowWithNonEmptyNameExists(content, objectID):
+    for line in content:
+        if line[0] == objectID and line[1] != "":
+            return True
+    return False
+
+def rowIndicesToRemove(content):
+
+    # ObjectIDs, ktere maji pouze prazdna jmena
+    objectIDsWithEmptyName = [] 
+
+    # Indexy radku k odstraneni
+    indices = []
+
+    # hledam radky k odstraneni
+    for index, line in enumerate(content):
+        if rowWithNonEmptyNameExists(content, line[0]) == False:
+            if line[0] in objectIDsWithEmptyName:
+                # radek druhy a nasledujici objekt s prazdnym jmenem oznacim k odstraneni
+                indices.append(index + 1)
+            else:
+                # prvni objekt s prazdnym jmenem musi zustat
+                objectIDsWithEmptyName.append(line[0])
+
+    return indices
 
 # open .tsv file
-with open("data_cleanup.tsv") as file:
-    tsv_file = csv.reader(file, delimiter="\t")
-    for line in tsv_file:
-        print(line)
+with open(os.path.dirname(__file__) + '/../../data_cleanup.tsv') as file:
+    content = csv.reader(file, delimiter="\t")
+    
+    # Nactu si cely soubor do promenne contentInMemory
+    contentInMemory = []    
+    for line in content:
+        contentInMemory.append(line)
+
+    # vypisu indexy radku k odstraneni
+    print(rowIndicesToRemove(contentInMemory))
