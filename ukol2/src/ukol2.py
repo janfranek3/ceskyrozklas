d9226bbb877fbb116d0ae504a712453a116ef40b
Jan Franek <janfranek3@gmail.com>
2024-09-15 21:19:41 +0200

    ukol2

--- ukol2/src/ukol2.py
+++ ukol2/src/ukol2.py
@@ -1,24 +1,43 @@
 import xml.etree.ElementTree as ET
 import csv
+import argparse
+import os
+import json
 
+# export do json
+def output_json(output_file, columns, values):
+    with open(os.path.dirname(__file__) + '/'+output_file, 'w', newline='') as file:
+        
+        output = {}
+        for i, column in enumerate(columns):
+            output[column] = values[i]
+        
+        json.dump(output, file)
 
-tree = ET.parse('obecnа_osoba.xml')
-root = tree.getroot()
+# export do csv
+def output_csv(output_file, columns, values):
+    with open(os.path.dirname(__file__) + '/'+output_file, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(columns)
+        writer.writerow(values)
 
+# export do tsv
+def output_tsv(output_file, columns, values):
+    with open(os.path.dirname(__file__) + '/'+output_file, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(columns)
+        writer.writerow(values)        
 
-def parseXML():
-    return
+# funkce zparsuje vstupni xml a vrati pole sloupcu a hodnot
+def parse_xml(input_file):
 
+    tree = ET.parse(os.path.dirname(__file__) + '/'+input_file)
+    root = tree.getroot()
 
-
-with open('export.csv', 'w', newline='') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    
     OM_OBJECT = root.find(".//OM_OBJECT")
     ObjectID = OM_OBJECT.get('ObjectID')
     DirectoryID = OM_OBJECT.get('DirectoryID')
     TemplateName = OM_OBJECT.get('TemplateName')
-
 
     columns = ['ObjectID', 'DirectoryID', 'TemplateName']
     values = [ObjectID, DirectoryID, TemplateName]
@@ -26,7 +45,6 @@ with open('export.csv', 'w', newline='') as csvfile:
     # Naplnim 
     for OM_FIELD in root.findall(".//OM_FIELD"):
         FieldID = OM_FIELD.get('FieldID')
-        IsEmpty = OM_FIELD.get('IsEmpty')
         OM_STRING = OM_FIELD.find('./OM_STRING')
     
         if OM_STRING is None:
@@ -39,8 +57,44 @@ with open('export.csv', 'w', newline='') as csvfile:
 
         columns.append(FieldID)
         values.append(value)
-    
-    spamwriter.writerow(columns)
-    spamwriter.writerow(values)
+        
+    return [columns, values]
+        
+
+# Hlavni funkce programu, ktera se spusti ihned jako prvni po spusteni
+def main():
+
+    # Zpracovani vstupnich parametru programu
+    parser = argparse.ArgumentParser(
+                        prog='2. Úkol',
+                        description='XML transformace',
+                        epilog='')
+
+    parser.add_argument('source_file')
+    parser.add_argument('--output_file')
+    parser.add_argument('--otype')
+
+    args = parser.parse_args()
+
+    if os.path.isfile(args.source_file) == False:
+        raise Exception("Input file does not exist")
+
+    if args.output_file is not None:
+        output_file_name = args.output_file
+    else:
+        output_file_name = os.path.basename(args.source_file).split('.')[0]
+
+    # Zparsovani vstupniho xml
+    parsed_xml = parse_xml(args.source_file)
+
+    # Export do prislusneho formatu
+    if args.otype == 'tsv':
+        output_tsv(output_file_name+'.output.tsv', parsed_xml[0], parsed_xml[1])
+    elif args.otype == 'json':
+        output_json(output_file_name+'.output.json', parsed_xml[0], parsed_xml[1])
+    else:
+        output_csv(output_file_name+'.output.csv', parsed_xml[0], parsed_xml[1])
 
 
+
+main()
